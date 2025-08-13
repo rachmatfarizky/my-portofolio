c4823ed1b240d1f128aec90da8a7cb152bd8bfea
Rachmat Farizky <rachmatfarizky@mail.ugm.ac.id>
2025-08-13 13:43:54 +0700

    Initial portfolio commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -212,7 +212,7 @@ export default function Home() {
 				<footer className="bg-gray-800 text-white">
 					<div className="container mx-auto px-6 py-8 text-center">
 						<p>
-							&copy; {new Date().getFullYear()} Rachmat Farizky. Dibuat dengan ❤️
+							&copy; {new Date().getFullYear()} Rachmat Farizky. Dibuat dengan  
 							dan semangat.
 						</p>
 					</div>
